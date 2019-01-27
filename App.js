412e779d6ea37faca888f6fa13eb29a5f7274c1e
Farid Faraji <faridfaraji@FAMP.local>
2019-01-26 19:22:56 -0800

    works-atleast

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Menu } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
 import UserLocation from './components/userloc.js'
 import AppDrawerNavigator from './components/sidebar.js'
 
 export default class App extends React.Component {
+
+  state = {
+    isLoadingComplete: false,
+  };
+
   render() {
+    global.radius = 1000;
 
 
    return (
